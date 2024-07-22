52946fdbf3d342913c0036d6cd31c8f7c51c181a
Dafne Huertas <nafechg02@gmail.com>
2024-07-22 15:48:17 -0500

    Huertas Examen Final-Parte 3-Formulario

--- examenfinal/examenfinal/urls.py
+++ examenfinal/examenfinal/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from miapp import views
@@ -22,4 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.index, name = "index"),
     path('personas/', views.personas, name = "personas"),
+    path('agregar_persona/', views.agregar_persona, name='agregar_persona'),
+    path('eliminar_persona/<int:id>/', views.eliminar_persona, name="eliminar_persona"),
+    path('editar_persona/<int:id>/', views.editar_persona, name='editar_persona'),
 ]
--- examenfinal/miapp/views.py
+++ examenfinal/miapp/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Huertas_Persona
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+
+def agregar_persona(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        apellidos = request.POST['apellidos']
+        sexo = request.POST['sexo']
+        persona = Huertas_Persona(nombre=nombre, apellidos=apellidos, sexo=sexo)
+        persona.save()
+        messages.success(request, 'La persona ha sido agregada con éxito.')
+        return redirect('personas')
+    return render(request, 'agregar_persona.html')
+
+def editar_persona(request, id):
+    persona = get_object_or_404(Huertas_Persona, id=id)
+    if request.method == 'POST':
+        persona.nombre = request.POST['nombre']
+        persona.apellidos = request.POST['apellidos']
+        persona.sexo = request.POST['sexo']
+        persona.save()
+        messages.success(request, 'La persona ha sido actualizada con éxito.')
+        return redirect('personas')
+    return render(request, 'editar_persona.html', {'persona': persona})
+
 def personas(request):
-    return render(request, 'personas.html')
+    personas = Huertas_Persona.objects.all()
+    return render(request, 'personas.html', {'personas': personas})
+
+def eliminar_persona(request, id):
+    persona = get_object_or_404(Huertas_PersonaPersona, id=id)
+    persona.delete()
+    messages.success(request, 'La persona ha sido eliminada con éxito.')
+    return redirect('personas')
