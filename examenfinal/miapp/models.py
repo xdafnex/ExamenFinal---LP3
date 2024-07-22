28d7fb81d2dd6a59ac2ae52deccc48dacac7abb2
Dafne Huertas <nafechg02@gmail.com>
2024-07-22 15:15:13 -0500

    Huertas Examen Final-Parte 2-Base de datos

--- examenfinal/miapp/models.py
+++ examenfinal/miapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Huertas_Persona(models.Model):
+    SEXO = [
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+    ]
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=1, choices=SEXO)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
